32fb9f33465d36e9ba3fff83bc0c66f0a337e781
Maxwell Gottesman <maxgman16@yahoo.com>
2022-11-20 23:03:58 -0500

    commit tree 62ab27b9721af5b7e524dee31a10c77dc51e6a71

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let j = 0;
+  for(let i = 0; i < array.length; ++i) {
+    for(; j < array.length; ++j) {
+      if ((array[i] + array[j] === target) && i !== j) { // i iterator will retreive the first value we're testing,
+        // j retrieves the second. See if they add up to the target
+        // also check if they are the same index
+        // since i and j are both pointing to the indexes, you can compare them directly
+        return true;
+      };
+    };
+    j = 0;
+  };
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  for each element(eleSuper) of the array
+    for each element(eleSub) of the array
+      if the sum of it and eleSuper is the target sum
+      and if indSuper is not indSub
+      return true
+  return false
 */
 
 /*
@@ -20,7 +39,12 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
+  console.log
+
   console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([10,1,2,3], 10));
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
